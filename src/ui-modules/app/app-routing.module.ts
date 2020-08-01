4b56715f2ce14be4516893dc6dc0e58984adaf9e
Jeevan Kumar <mdjeevankumar@gmail.com>
2020-08-02 00:55:47 +0530

    folder struct

--- src/ui-modules/app/app-routing.module.ts
+++ src/ui-modules/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { AuthGuard } from 'src/services/Auth/auth.guard';
+import { LoginButtonComponent } from '../login-button/login-button.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginButtonComponent,
+    canActivate: [AuthGuard]
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
